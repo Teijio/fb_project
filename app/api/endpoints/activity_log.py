ef0abdae38e9fe122d29b5699d08a09b0cb99e27
Teijio <ridpfrep@yandex.ru>
2024-04-14 22:06:25 +0300

    logging

--- app/api/endpoints/activity_log.py
+++ app/api/endpoints/activity_log.py
@@ -1,3 +1,4 @@
+import logging
 import ipaddress
 
 from fastapi import APIRouter, Depends, Request
@@ -5,14 +6,20 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.httpx_client import Singletonhttpx
 from app.api.utils import generate_facebook_event_data
-from app.api.validators import (check_unique_ip_address, extract_keitaro_info,
-                                get_activity_log, get_pixel_token)
+from app.api.validators import check_unique_ip_address, extract_keitaro_info, get_activity_log, get_pixel_token
 from app.core.database import get_async_session
 from app.crud.activity_log import activity_log_crud
 from app.schemas.facebook_data import ActivityLogCreate, KeitaroStatusIP
 
 router = APIRouter()
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler("/app/logs/logs.log")
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 @router.post("/create/")
 async def create_new_request_info(
@@ -21,6 +28,7 @@ async def create_new_request_info(
     session: AsyncSession = Depends(get_async_session),
 ):
     activity_log.ip_address = ipaddress.ip_address(request.client.host)
+    logger.info(f"facebook_log >>> {activity_log}")
     await check_unique_ip_address(activity_log.ip_address, session)
     new_activity_log = await activity_log_crud.create(activity_log, session)
     return new_activity_log
@@ -31,9 +39,12 @@ async def find_request_info(
     info_url: KeitaroStatusIP,
     session: AsyncSession = Depends(get_async_session),
 ):
+    logger.info(f"keitaro_URL >>> {info_url}")
     ip_address, status = extract_keitaro_info(info_url)
     activity_log = await get_activity_log(ip_address, session)
     pixel_token = await get_pixel_token(activity_log.pixel, session)
     facebook_data = generate_facebook_event_data(activity_log, status)
+    logger.info(f"keitaro_POST >>> {info_url}")
     result = await Singletonhttpx.send_data_to_facebook(pixel_token.pixel, pixel_token.token, facebook_data)
+    logger.info(f"keitaro_RESULT >>> {info_url}")
     return result
--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
+import logging
+
 import uvicorn
 from fastapi import FastAPI
 
 from app.api.httpx_client import on_shutdown, on_start_up
 from app.api.routers import main_router
 from app.core.config import settings
+
+logging.basicConfig(filename="/app/logs/logs.log", level=logging.INFO)
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 app = FastAPI(title=settings.app_title, on_startup=[on_start_up], on_shutdown=[on_shutdown])
 app.include_router(main_router)
